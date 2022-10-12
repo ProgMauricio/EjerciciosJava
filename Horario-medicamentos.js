a3baebdab0e0335752ccde6be136f2baf8954a91
ProgMauricio <perezmtzm30@gmail.com>
2022-10-12 11:01:43 -0500

    Âeste es un avance del trabajo

--- Horario-medicamentos.js
+++ Horario-medicamentos.js
@@ -1,1 +1,19 @@
-console.log("tipo de dato")
+console.log("Horario de toma de medicamentos");
+
+let hora = prompt("Cual es la hora inicial que debe tomar su medicamento");
+
+let frecuencia = prompt("Con que frecuencia debe tomarse el medicamento")
+
+
+/* Ver porque falla */
+/* for(let hora; + frecuencia) {
+    document.write("La hora inicial que tomara su medicamento es a la(s)")
+} */
+
+
+
+/* let h */
+
+/* document.write("hola") */
+
+

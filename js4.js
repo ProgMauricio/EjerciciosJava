d4823f87a9bf101fd8c55b732ce8c7e95ba917d4
ProgMauricio <perezmtzm30@gmail.com>
2022-10-17 09:36:19 -0500

    modifique el archivo

--- js4.js
+++ js4.js
@@ -11,4 +11,60 @@ function divisores(numero) {
     return total;
 }
 
+
+
+
+const divisoresV2 = function(numero){
+    let total = 0;
+
+    for(let i=1; i <=numero; i++) {
+        if(numero % i == 0){
+            total++;
+        }
+
+    }
+    return total;
+};
+
+
+
+
+
+
+
+const divisoresV3 = numero => {
+    let total = 0;
+
+    for(let i=1; i <=numero; i++) {
+        if(numero % i == 0){
+            total++;
+        }
+
+    }
+    return total;
+};
+
 console.log("Total de divisores", divisores(32));
+console.log("Total de divisores", divisoresV2(32));
+console.log("Total de divisores", divisoresV3(32));
+
+
+
+/* function f1(){}
+
+function f2(){}
+
+function f3(){} */
+
+/* ejemplo = function(numero){
+        let total = 0;
+    
+        for(let i=1; i <=numero; i++) {
+            if(numero % i == 0){
+                total++;
+            }
+    
+        }
+        return total;
+    
+} */

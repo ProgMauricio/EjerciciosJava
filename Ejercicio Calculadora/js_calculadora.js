a844e6786fdee2c3a9f8a0b295ae28b77a4a1b63
ProgMauricio <perezmtzm30@gmail.com>
2022-10-28 12:01:20 -0500

    actualizacion

--- Ejercicio Calculadora/js_calculadora.js
+++ Ejercicio Calculadora/js_calculadora.js
@@ -7,7 +7,44 @@
 
 
 /* function calcular(numero) { */
-document.querySelector('#calcular2').addEventListener('click', () =>{
+function calcular() {
+    /* document.querySelector('#calcular2').addEventListener('click', () =>{ */
+    n1 = parseInt(document.getElementsByClassName('numero1').value);
+    n2 = parseInt(document.getElementsByClassName('numero2').value);
+    op = document.getElementById('operacion').value;
+    let r;
+    if(op == '+'){
+        r = n1 + n2;
+    }else if( op == '-'){
+        r = n1 - n2;
+    }else if(op == '*'){
+        r = n1 * n2;
+    }else if(op == '/'){
+        r = n1 / n2;
+    }
+        document.querySelector('numresul')= r;
+        resultado.value = r;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*     document.querySelector('#calcular2').addEventListener('click', () =>{
     const n1 = parseInt(document.querySelector('#n11').value);
         const n2 = parseInt(document.querySelector('#n22').value);
         const op2 = document.querySelector('#op').value;
@@ -22,4 +59,4 @@ document.querySelector('#calcular2').addEventListener('click', () =>{
             r = n1 / n2;
         }
         document.querySelector('#r2').innerHTML = r;
-    });
+    }); */

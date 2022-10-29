3bb7a6d8350abd2dae4e92a1cfc3d52ec664f28f
ProgMauricio <perezmtzm30@gmail.com>
2022-10-28 22:42:28 -0500

    avance

--- Programa Tabla/Tabla.js
+++ Programa Tabla/Tabla.js
@@ -1,1 +1,19 @@
-funcion multiplicar() {}
+function generar() {
+        let tabla= document.getElementById('tabla').value;
+        let longitud= document.getElementById('longitud').value;
+        let tipo= document.getElementById('tipo').value;
+     if (tipo == 'normal') {
+        for (i=0; i<= longitud; i++) {
+            document.write(""+tabla+" X "+i+" = "+(i*tabla)+"<br>");
+        }
+     }
+     else {
+        for (i=0; i> longitud; i--) {
+            document.write(""+tabla+" X "+i+" = "+(i*tabla)+"<br>");
+     }
+    }
+    /* tabla.innerHTML =  */
+}
+
+
+/* i=longitud; i> longitud; i-- */

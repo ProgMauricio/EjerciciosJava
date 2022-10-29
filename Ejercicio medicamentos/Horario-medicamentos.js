1bd9f979baffd85cf5ba6fc04f3da579e3e62c3e
ProgMauricio <perezmtzm30@gmail.com>
2022-10-28 20:37:46 -0500

    actualize el js4 y ejercicio de tabla

--- Ejercicio medicamentos/Horario-medicamentos.js
+++ Ejercicio medicamentos/Horario-medicamentos.js
@@ -1,27 +1,3 @@
-/* 
-console.log("Horario de toma de medicamentos");
-
-let hora = prompt("Cual es la hora inicial que debe tomar su medicamento");
-
-let frecuencia = prompt("Con que frecuencia debe tomarse el medicamento")
- */
-
-/* Ver porque falla */
-/* for(let hora; + frecuencia) {
-    document.write("La hora inicial que tomara su medicamento es a la(s)")
-} */
-
-
-
-/* let h */
-
-/* document.write("hola") */
-
-/* console.log(`tu la hora inicial para tomar tu medicamento es a las`) */
-
-
-
-
 let hora= Number(prompt("¿Cual es la hora inicial que debe tomar su medicamento?"));
 let frecuencia= Number(prompt("¿Con que frecuencia debe tomarse el medicamento?"));
 let dias= Number(prompt("¿Por cuantos dias tiene que tomar el medicamento?"));
